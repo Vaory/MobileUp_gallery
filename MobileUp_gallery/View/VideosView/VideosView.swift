e0d425d434c9cfa3cc0954b559c26cbd6b6b3e30
Mikhail Demichev <demichev.mi@gmail.com>
2024-08-27 00:20:03 +0300

    Permission received from VK to access video, video display fixed

--- MobileUp_gallery/View/VideosView/VideosView.swift
+++ MobileUp_gallery/View/VideosView/VideosView.swift
@@ -11,44 +11,41 @@ struct VideosView: View {
     @StateObject private var viewModel = VideosViewModel()
 
     var body: some View {
-       
-            ScrollView {
-                LazyVStack(spacing: 4) {
-                    ForEach(viewModel.videos) { video in
-                        ZStack(alignment: .bottomLeading) {
-                            AsyncImage(url: URL(string: video.thumbnailUrl)) { image in
-                                image
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fill)
-                                    .frame(maxWidth: .infinity, maxHeight: 210)
-                                    .clipped()
-                            } placeholder: {
-                                ProgressView()
-                                    .frame(maxWidth: .infinity, maxHeight: 210)
-                            }
-
-                            Text(video.title)
-                                .foregroundColor(.black)
-                                .padding(.horizontal, 15)
-                                .padding(.vertical, 4)
-                                .background(Color.white.opacity(0.5))
-                                .cornerRadius(16)
-                                .padding([.bottom, .trailing], 16)
+        ScrollView {
+            LazyVStack(spacing: 4) {
+                ForEach(viewModel.videos) { video in
+                    ZStack(alignment: .bottomTrailing) { // Align to the bottom right
+                        AsyncImage(url: URL(string: video.thumbnailUrl)) { image in
+                            image
+                                .resizable()
+                                .aspectRatio(contentMode: .fill)
+                                .frame(maxWidth: .infinity, maxHeight: 210)
+                                .clipped()
+                        } placeholder: {
+                            ProgressView()
+                                .frame(maxWidth: .infinity, maxHeight: 210)
                         }
-                        .background(Color.white)
-                        .frame(width: 210)
+
+                        Text(video.title)
+                            .foregroundColor(.black)
+                            .padding(.horizontal, 15)
+                            .padding(.vertical, 4)
+                            .background(Color.white.opacity(0.5))
+                            .cornerRadius(16)
+                            .padding([.bottom, .trailing], 16) // Ensure padding is on the bottom and trailing edges
                     }
-                }
-                .padding()
-            }
-            .onAppear {
-                if let token = UserDefaults.standard.string(forKey: "VKAccessToken") {
-                    viewModel.fetchVideos(token: token)
-                } else {
-                    print("Access token not found")
+                    .frame(height: 210)
+                    
                 }
             }
-        
+        }
+        .onAppear {
+            if let token = UserDefaults.standard.string(forKey: "VKAccessToken") {
+                viewModel.fetchVideos(token: token)
+            } else {
+                print("Access token not found")
+            }
+        }
         .ignoresSafeArea(.all)
     }
 }
@@ -56,6 +53,7 @@ struct VideosView: View {
 struct VideosView_Previews: PreviewProvider {
     static var previews: some View {
         VideosView()
-        
     }
 }
+
+
